e6262286219e2c3f675168b6441ba6c49d295bea
preethi-r04 <preethiliki2004@gmail.com>
2026-05-01 22:48:43 +0530

    [Preethi] : Added : UC8 : Secure note creation using JWT principal

---
+++ src/main/java/com/fundoo/notes/dto/NoteRequest.java
@@ -0,0 +1,9 @@
+package com.fundoo.notes.dto;
+
+import lombok.Data;
+
+@Data
+public class NoteRequest {
+    private String title;
+    private String description;
+}
---
+++ src/main/java/com/fundoo/notes/service/NoteService.java
@@ -0,0 +1,7 @@
+package com.fundoo.notes.service;
+
+import com.fundoo.notes.dto.NoteRequest;
+
+public interface NoteService {
+    String createNote(NoteRequest request, String email);
+}
